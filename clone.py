fa88a2557b22199462cc26ab0ae3675f43b46122
Ricardo Mota <ricardosllm@gmail.com>
2017-07-26 09:19:28 +0800

    Added data normalization step

--- clone.py
+++ clone.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda
 
 
 lines = []
@@ -27,10 +27,16 @@ X_train = np.array(images)
 y_train = np.array(mesurements)
 
 model = Sequential()
-model.add(Flatten(input_shape=(160, 320, 3)))
+# Normalize the data
+model.add(Lambda(lambda x: x / 255.0 - 0.5,
+                 input_shape=(160, 320, 3)))
+model.add(Flatten())
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epochs=7)
+model.fit(X_train, y_train,
+          validation_split=0.2,
+          shuffle=True,
+          nb_epochs=7)
 
 model.save('model.h5')
