0e76e5faa9d7ea8bbbbb2d3093ad7a52d12129bc
Ricardo Mota <ricardosllm@gmail.com>
2017-07-28 21:05:21 +0800

    Tried another model, NVIDIA's

--- clone.py
+++ clone.py
@@ -41,19 +41,33 @@ model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5,
                  input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((70,25),(0,0))))
-model.add(Convolution2D(6, 5, 5, activation='relu'))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6, 5, 5, activation='relu'))
-model.add(MaxPooling2D())
+
+# NVIDIA architecture
+model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(48,5,5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(64,3,3, activation='relu'))
+model.add(Convolution2D(64,3,3, activation='relu'))
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
+
+# LeNet
+# model.add(Convolution2D(6, 5, 5, activation='relu'))
+# model.add(MaxPooling2D())
+# model.add(Convolution2D(6, 5, 5, activation='relu'))
+# model.add(MaxPooling2D())
+# model.add(Flatten())
+# model.add(Dense(120))
+# model.add(Dense(84))
+# model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train,
           validation_split=0.2,
           shuffle=True,
-          epochs=10)
+          epochs=5)
 
 model.save('model.h5')
