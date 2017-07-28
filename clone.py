77d989ce70b2522618d20f2d09971e9fb2882cb2
Ricardo Mota <ricardosllm@gmail.com>
2017-07-28 11:55:06 +0000

    Added data augmentation step

--- clone.py
+++ clone.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
@@ -17,21 +17,30 @@ with open('../data/driving_log.csv') as csvfile:
 images = []
 mesurements = []
 for line in lines:
-    source_path = line[0]
-    filename = source_path.split('/')[-1]
-    current_path = '../data/IMG/' + filename
-    image = cv2.imread(current_path)
-    images.append(image)
-    mesurement = float(line[3])
-    mesurements.append(mesurement)
+    for i in range(3):
+        source_path = line[i]
+        filename = source_path.split('/')[-1]
+        current_path = '../data/IMG/' + filename
+        image = cv2.imread(current_path)
+        images.append(image)
+        mesurement = float(line[3])
+        mesurements.append(mesurement)
 
-X_train = np.array(images)
-y_train = np.array(mesurements)
+augmented_images, augmented_mesurements = [], []
+for image, mesurement in zip(images, mesurements):
+    augmented_images.append(image)
+    augmented_mesurements.append(mesurement)
+    augmented_images.append(cv2.flip(image, 1))
+    augmented_mesurements.append(mesurement*-1.0)
+
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_mesurements)
 
 model = Sequential()
 # Normalize the data
 model.add(Lambda(lambda x: x / 255.0 - 0.5,
                  input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70,25),(0,0))))
 model.add(Convolution2D(6, 5, 5, activation='relu'))
 model.add(MaxPooling2D())
 model.add(Convolution2D(6, 5, 5, activation='relu'))
@@ -45,6 +54,6 @@ model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train,
           validation_split=0.2,
           shuffle=True,
-          epochs=7)
+          epochs=10)
 
 model.save('model.h5')
